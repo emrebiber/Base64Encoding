c66ebb7e25598d9c8eda71ad5fae0032e3492baf
Emre Biber <e.biber@trenkwalder.com>
2021-09-08 16:54:18 +0300

    added comments

--- Base64Encoding/Program.cs
+++ Base64Encoding/Program.cs
@@ -7,14 +7,18 @@ namespace Base64Encoding
     {
         static void Main(string[] args)
         {
+            byte[] bytes;
+
+            // encoding string
             var plainText = "Hello, World!";
 
-            byte[] bytes = Encoding.ASCII.GetBytes(plainText);
+            bytes = Encoding.ASCII.GetBytes(plainText);
             string encodedBase64 = Convert.ToBase64String(bytes);
 
             Console.WriteLine($"Plain Text: {plainText}");
             Console.WriteLine($"Encoded Text: {encodedBase64}");
 
+            // decoding string
             var encodedText = "SGVsbG8sIFdvcmxkIQ==";
 
             bytes = Convert.FromBase64String(encodedText);
