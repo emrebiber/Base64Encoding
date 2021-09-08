a4d508f1eff03197d274f3e254aeb286bd84d236
Emre Biber <e.biber@trenkwalder.com>
2021-09-08 11:42:01 +0300

    initial commit

---
+++ Base64Encoding/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Base64Encoding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
