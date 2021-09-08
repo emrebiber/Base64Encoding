28d0b607bfb4a227ab62e0c63990c61ae7ad63eb
Emre Biber <e.biber@trenkwalder.com>
2021-09-08 12:01:36 +0300

    base64 encoding/decoding example

--- Base64Encoding/Program.cs
+++ Base64Encoding/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace Base64Encoding
 {
@@ -6,7 +7,18 @@ namespace Base64Encoding
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            byte[] bytes = Encoding.ASCII.GetBytes("emrebiber");
+            string base64String = Convert.ToBase64String(bytes);
+
+            Console.WriteLine(base64String);
+
+            // decoding starts here
+            bytes = Convert.FromBase64String(base64String);
+            var decoded = Encoding.ASCII.GetString(bytes);
+
+            Console.WriteLine(decoded);
+
+            Console.ReadKey();
         }
     }
 }
